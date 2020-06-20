8b9d792e5a59dcf0b87ee6fe58ace5d6b99aa544
lee byeonghoon <qudgns200@gmail.com>
2020-06-21 02:03:08 +0900

    modify View

--- portfolio/src/main/java/io/github/qudgns200/controller/ApiController.java
+++ portfolio/src/main/java/io/github/qudgns200/controller/ApiController.java
@@ -1,18 +1,26 @@
 package io.github.qudgns200.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import io.github.qudgns200.model.User;
+import io.github.qudgns200.service.UserService;
 
 @CrossOrigin(origins = "*") // Cross-Origin Resource Sharing 요청 처리를 위함
 @RestController
-public class ApiController {	
+public class ApiController {
 	
-	@RequestMapping("/Hello")
-	public User hello() {
-		User p = new User();
-		return p;
+	@Autowired
+	private UserService uService;
+	
+	// User 정보 가져오기
+	@RequestMapping(value="/User/1", method= {RequestMethod.GET})
+	public User getUser(String email) {
+		User user = new User();
+		user = uService.getUesrInfo(email);
+		return user;
 	}
 }
